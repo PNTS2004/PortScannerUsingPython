1b1fd0a1062c2f3db04c2b1dbb2c2caac858b11b
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-04-28 13:49:59 -0400

    Accepts multiple (and single) IPs and Doamin names as target

--- PortScanner/portscanner.py
+++ PortScanner/portscanner.py
@@ -1,12 +1,34 @@
 import socket
 from IPy import IP
 
-ipaddress=input('Enter the Target IP: ')
-port=int(input('Enter the Port to be scanned: '))
+def scan(target):
+    converted_ip=check_ip(target)
+    print('\n', 'Scanning Ports:-')
+    for port in range(a,b+1):
+        scan_port(converted_ip, port)
 
-try:
-    sock=socket.socket()
-    sock.connect((ipaddress, port))
-    print('Port '+str(port)+' is Open, Congratulations!')
-except:
-    print('Port '+str(port)+' is Closed. Better luck next time.')
+def check_ip(ip):
+    try:
+        IP(ip)
+        return(ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
+def scan_port(ipaddress,port):
+    try:
+        sock=socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
+        print('Port '+str(port)+' is Open')
+    except:
+        pass
+
+targets = input('Enter the Target/s IP (split different targets using ,): ')
+a = int(input('Enter the starting port of the range: '))
+b = int(input('Enter the sending port of the range: '))
+
+if ',' in targets:
+    for ip_add in targets.split(','):
+        scan(ip_add.strip())
+else:
+    scan(targets)
